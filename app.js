0409c1a99a4ec3d1ee86f628d2bc9f26574940a1
Martin Sieniawski <m.sieniawski@live.ca>
2015-05-11 15:39:42 -0400

    Added prototype logic

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger     = require('morgan');
 var path       = require('path');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+
 var app = express();
 
 /* Configure Application */
@@ -15,6 +18,7 @@ app.set('view engine', 'html');
 app.set('layout', 'layout');
 app.engine('html', hogan);
 app.set('views', path.join(__dirname, 'views'));
+app.set('socket', 'mongodb://localhost:27017/cruel');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
@@ -25,6 +29,48 @@ app.use(bodyParser.urlencoded());
 /* Load Routes */
 app.get('/', function(req, res) {
     res.render('index', {});
+});
+
+app.get('/compose', function(req, res) {
+    res.render('compose', {});
+});
+
+app.get('/feed', function(req, res) {
+    /* Load the feed data first */
+    var db = monk(app.get('socket'));
+    var posts = db.get('posts');
+    posts.find({}).on('success', function(doc) {
+        db.close();
+        res.render('feed', {
+            initData: JSON.stringify({data: [doc]})
+        });
+    });
+});
+
+app.post('/post', function(req, res) {
+    // Store the data in the db
+    var db = monk(app.get('socket'));
+    var posts = db.get('posts');
+    posts.insert(req.body).on('success', function(doc) {
+        db.close();
+        res.redirect('/feed');
+    });
+});
+
+/* Handle 404 */
+app.use(function(req, res, next) {
+    var err = new Error('Not Found!');
+    err.status = 404;
+    next(err);
+});
+
+/* Handle errors at the end of the chain */
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
 });
 
 app.listen(3000, function() {
