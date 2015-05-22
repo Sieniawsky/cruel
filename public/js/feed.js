01a85aa8117a6bc28d2df7b77f66d3e7397d544d
Martin Sieniawski <m.sieniawski@live.ca>
2015-05-22 16:21:17 -0400

    #13 - Moved all feed js over to backbone

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,43 +1,128 @@
-/* Initialize the feed */
+/* Experiment with Backbone */
 $(function() {
 
-    /* Hook scroll for async feed load */
-    var page = 1;
-    var has_next = true;
-    $(window).scroll(function() {
-        if (($(window).scrollTop() == $(document).height() - $(window).height()) && has_next) {
-            var last_id = initData.data[initData.data.length - 1]._id;
-            loadPosts(page, last_id);
-            page++;
+    /* Post Model */
+    var Post = Backbone.Model.extend({
+        default: {
+            done: false
+        },
+
+        initialize: function() {}
+    });
+
+    /* Post Collection */
+    var PostList = Backbone.Collection.extend({
+        model: Post
+    });
+
+    /* Post View */
+    var PostView = Backbone.View.extend({
+
+        tagName: 'div',
+
+        template: _.template($('#post-template').html()),
+
+        events: {
+            'click .js-like': 'like'
+        },
+
+        initialize: function() {},
+
+        like: function() {
+            console.log('Like has been clicked');
+            if (typeof initData.user._id !== "undefined" && initData.user._id !== null) {
+                $.ajax({
+                    url : '/post/like',
+                    type : 'POST',
+                    data : {
+                        post : this.model.attributes,
+                        user : initData.user
+                    },
+                    success : function(data) {},
+                    failure : function(data) {}
+                });
+            } else {
+                // Prompt user to login
+            }
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
         }
     });
 
-    /* Asynchronously loads the next page of posts */
-    var loadPosts = function(page, last) {
-        // AJAX TINGZ
-        $.ajax({
-            url : '/' + page + '/' + last,
-            type : 'GET',
-            success : function(data) {
+    /* Application View */
+    var AppView = Backbone.View.extend({
 
-                if (data.length < 8) has_next = false;
+        el: '.js-app',
 
-                var post_template = _.template($('#post-template').html());
-                var posts = $('.js-posts');
-                _.each(data, function(elem) {
-                    posts.append(post_template(elem));
-                });
+        initialize: function() {
+            this.$feed = $('.js-feed');
+            this.triggerPoint = 100;
+            this.page = 0;
+            this.isLoading = false;
+            this.hasMore = true;
 
-                if (data.length < 8) {
-                    has_next = false;
-                    var completed_template = _.template($('#completed-template').html());
-                    $('.js-posts').append(completed_template());
-                }
-            },
-            failure : function(data) {
-                var warning_template = _.template($('#warning-template').html());
-                $('.js-posts').append(warning_template());
+            this.completed_template = _.template($('#completed-template').html());
+
+            _.bindAll(this, 'checkScroll');
+            $(window).scroll(this.checkScroll);
+
+            this.posts = new PostList();
+            var that = this;
+            _.each(initData.data, function(post) {
+                that.posts.add(new Post(post));
+            });
+
+            this.addAll();
+        },
+
+        addOne: function(post) {
+            var view = new PostView({model: post});
+            this.$feed.append(view.render().el);
+        },
+
+        addAll: function() {
+            this.posts.each(this.addOne, this);
+        },
+
+        checkScroll: function() {
+            if (!this.isLoading && this.hasMore &&
+                ($(window).scrollTop() + this.triggerPoint
+                    > $(document).height() - $(window).height())) {
+                this.page += 1;
+                this.loadPosts();
             }
-        });
-    };
+        },
+
+        loadPosts: function() {
+            this.isLoading = true;
+
+            var last = this.posts.at(this.posts.length - 1).get('_id');
+            var that = this;
+            $.ajax({
+                url: '/' + this.page + '/' + last,
+                type: 'GET',
+                success: function(data) {
+                    if (data.length < 8) that.hasMore = false;
+                    _.each(data, function(post) {
+                        that.posts.add(new Post(post));
+                    })
+
+                    if (data.length < 8) {
+                        that.hasMore = false;
+                        that.$feed.append(that.completed_template());
+                    }
+
+                    that.isLoading = false;
+                },
+                failure: function(data) {
+                    that.isLoading = false;
+                }
+            });
+        }
+    });
+
+    var app = new AppView;
 });
