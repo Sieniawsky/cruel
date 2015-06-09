d4977202b3faaa38bdcc921317185caa26f81b37
Martin Sieniawski <m.sieniawski@live.ca>
2015-06-09 12:38:31 -0400

    #41 - Modified build script to build all source bundles, customized UserFeed.js

--- assets/UserFeed.js
+++ assets/UserFeed.js
@@ -10,5 +10,38 @@ var FeedView       = require('./views/FeedView');
 
 /* Feed for a user's profile page */
 var UserFeed = FeedView.extend({
-    
+
+    events: {
+        'change .js-sort'     : 'load',
+        'change .js-location' : 'load'
+    },
+
+    initialize: function() {
+        /* Call super initialize */
+        FeedView.prototype.initialize.apply(this);
+
+        this.$sort     = $('.js-sort');
+        this.$location = $('.js-location');
+
+        // Sorting options
+        this.sort     = 'new';
+        this.location = this.$location.val();
+    },
+
+    load: function() {
+        this.sort = this.$sort.val();
+        this.location = this.$location.val();
+        this.page = 1;
+        this.hasMore = true;
+        this.posts.fetch({
+            reset: true,
+            sort: this.sort,
+            location: this.location
+        });
+    } 
+});
+
+/* Start it up */
+$(function() {
+    var feed = new UserFeed();
 });
