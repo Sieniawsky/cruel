e9322180f0ccb1f552a4cceb162e8922ae0ac8bb
Martin Sieniawski <m.sieniawski@live.ca>
2015-06-15 16:37:02 -0400

    Added heroku database socket

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     'devSocket'  : 'mongodb://localhost:27017/cruel',
-    'prodSocket' : 'mongodb://localhost:27017/prod'
+    'prodSocket' : 'mongodb://swaglord69:myballs@ds047722.mongolab.com:47722/heroku_k2lqlzp8'
 }
