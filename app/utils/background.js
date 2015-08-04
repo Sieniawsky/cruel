796306b1110bdb6548b6cdfd74c0f0b0a088738f
Martin Sieniawski <m.sieniawski@live.ca>
2015-08-04 13:23:26 -0400

    #293 - Added new background system via manifest

--- app/utils/background.js
+++ app/utils/background.js
@@ -5,10 +5,6 @@ var path = require('path');
 /* Returns the file name of a random
    backgrounds image for use during rendering */
 module.exports = function() {
-    var files = fs.readdirSync(path.join(__dirname, '../../public/images/backgrounds'));
-    /* Filter our dot files */
-    var filtered = _.filter(files, function(file) {
-        return file.charAt(0) !== '.';
-    });
-    return filtered[Math.floor(Math.random() * filtered.length)];
+    var backgrounds = require('../../assets/background.json').images;
+    return backgrounds[Math.floor(Math.random() * backgrounds.length)];
 };
