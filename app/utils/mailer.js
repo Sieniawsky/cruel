13a81e5f1a6aee84064a3bf4c2649ef646637cfe
joecornfield <josephcornfield@gmail.com>
2015-09-14 23:29:40 -0400

    #434 - mailer messages

--- app/utils/mailer.js
+++ app/utils/mailer.js
@@ -1,23 +1,24 @@
 var _      = require('lodash');
 var mailer = require('nodemailer');
 
-var from = 'hello@cruel.co';
+var username = 'hello@cruel.co';
 var pass = '4kjj==311';
 
 var outgoing = function(target, subject, body, next) {
     var transport = mailer.createTransport({
         service : 'Gmail',
         auth    : {
-            user : from,
+            user : username,
             pass : pass
         }
     });
 
     var options = {
-        from    : from,
+        from    : 'Cruel Co <hello@cruel.co>',
         to      : target,
         subject : subject,
-        text    : body
+        text    : body,
+        html    : html
     };
 
     transport.sendMail(options, function(err, res) {
@@ -27,8 +28,8 @@ var outgoing = function(target, subject, body, next) {
 };
 
 var welcome = function(user, next) {
-    var subject = 'Welcome to Cruel!';
-    var body = 'Talk to me duggy';
+    var subject = 'Welcome to Cruel! Claim your free sticker pack.';
+    var html = '<p>Thanks for joining Cruel! Cruel.co is a web app where you can share and vote on content with other students on your campus. Make sure you check out <a href="https://cruel.co/welcome" target="_blank">this post</a> to see how it works!</p><p>Every week we give away prizes to active users on Cruel.co. As one of our very first users, you\'ve already won a free Cruel sticker pack! Simply respond to this e-mail with your name and address to get your stickers in the mail!</p>';
     var data = {};
     var template = _.template(body);
     outgoing(user.email, subject, template(data), next);
@@ -50,7 +51,7 @@ var forgot = function(email, host, token, next) {
     var body = 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
           'http://<%= host %>/reset/<%= token %>\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n';
     var data = {
         host: host,
         token: token
@@ -62,7 +63,7 @@ var forgot = function(email, host, token, next) {
 var reset = function(email, next) {
     var subject = 'Cruel.co Successful Password Reset';
     var body = 'Hello,\n\n' +
-          'This is a confirmation that the password for your account <%= email %> has just been changed.\n'
+          'This is a confirmation that the password for your account <%= email %> has just been changed.\n';
     var data = {
         email: email
     };
